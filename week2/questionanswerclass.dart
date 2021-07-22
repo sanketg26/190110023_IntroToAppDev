a504d2b84a48dc56fda0c0111c0ad54b6912dfd0
sanketg26 <87482229+sanketg26@users.noreply.github.com>
2021-07-22 20:56:30 +0530

    Delete questionanswerclass.dart
    already included in lib folder

--- week2/questionanswerclass.dart
+++
@@ -1,8 +0,0 @@
-class Question{
-  String questionText = "";
-  bool correctAnswer = true;
-  Question({required String ques, required bool ans}){
-    questionText = ques;
-    correctAnswer = ans;
-  }
-}
